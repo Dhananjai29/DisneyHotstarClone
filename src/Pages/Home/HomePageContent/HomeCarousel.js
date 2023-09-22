fa39b8dc68a33764470f2e5888f07c5741ec26e1
PrathviSahu <prathvisahu31@gmail.com>
2023-09-22 12:10:18 +0530

    justsamechangesprathvi

--- src/Pages/Home/HomePageContent/HomeCarousel.js
+++ src/Pages/Home/HomePageContent/HomeCarousel.js
@@ -8,22 +8,23 @@ const HomeCarousel = () => {
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <div className="img">
-                        
-                        <img src="https://img10.hotstar.com/image/upload/f_auto,q_90,w_1200/sources/r1/cms/prod/2423/1532423-i-784105888c43"  alt="..." />
+                        {/* atrangi re  */}
+                        <img src="https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/7516/1597516-i-751da827e22b"  alt="..." />
                     </div>
 
                     <div className="details">
                         <div className="posterLogo">
-                            <img src="https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/2425/1532425-t-90c8387aff87" alt="" />
+                            <img src="https://img10.hotstar.com/image/upload/f_auto,h_136/sources/r1/cms/prod/7515/1597515-t-efc19211a9e7" alt="" />
                         </div>
                         <div className="timing">
-                            <h3>2019 &#8901; 1h23m &#8901; Hindi <span>U/A 16+</span></h3>
+                            <h3>2021 &#8901; 2h16m &#8901; 2 Languages <span>U/A 13+</span></h3>
                         </div>
                         <div className="desc">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, vel. Facilis impedit consectetur molestiae doloribus.</p>
+                            
+                            <p>Runaway Rinkus gets forcefully married to an already engaged Vishu. The Love Triangle gets weider when Rinku's lover Sajjad shows up.</p>
                         </div>
                         <div className="genre">
-                            <p>Action &nbsp; | &nbsp;Spy&nbsp; | &nbsp;Buddy Cop&nbsp; | &nbsp;Chase</p>
+                            <p>Romance  &nbsp; | &nbsp;Drams&nbsp; | &nbsp;Couples&nbsp; | &nbsp;Comedy</p>
                         </div>
                         <div className="button">
                             <button className='watch-btn'> &#10148; &nbsp; Watch Now</button>
@@ -33,20 +34,21 @@ const HomeCarousel = () => {
                 </div>
                 <div class="carousel-item">
                     <div className="img">
-                    <img src="https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/1818/1571818-i-3171f7e60375"  alt="..." />
+                        {/* dil bechara  */}
+                    <img src="https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/8034/1408034-i-ef9ca05f9ecd"  alt="..." />
                     </div>
                     <div className="details">
                         <div className="posterLogo">
-                            <img src="https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/1814/1571814-t-c7e54c50b1a6" alt="" />
+                            <img src="https://img10.hotstar.com/image/upload/f_auto,h_136/sources/r1/cms/prod/8036/1408036-t-707a9b5521ad" alt="" />
                         </div>
                         <div className="timings">
                             <h3>2019 1h23m Hindi <span>U/A 16+</span></h3>
                         </div>
                         <div className="desc">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, vel. Facilis impedit consectetur molestiae doloribus.</p>
+                            <p>Brought together by a tragic twist,Kizie and Manny embark on a bitter sweet journey into the heart of that crazy little things called 'life'.</p>
                         </div>
                         <div className="genre">
-                            <p>Action &nbsp; | &nbsp;Spy&nbsp; | &nbsp;Buddy Cop&nbsp; | &nbsp;Chase</p>
+                            <p>Romance &nbsp; | &nbsp;Drama&nbsp; | &nbsp;Chick Flick&nbsp; | &nbsp;Rom com</p>
                         </div>
                         <div className="button">
                             <button className='watch-btn'> &#10148; &nbsp; Watch Now</button>
@@ -56,11 +58,12 @@ const HomeCarousel = () => {
                 </div>
                 <div class="carousel-item">
                     <div className="img">
-                    <img src="https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/5384/1375384-i-839823cdb057"  alt="..." />
+                        {/* Big bull */}
+                    <img src="https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/4935/1594935-i-295959e3ab60"  alt="..." />
                     </div>
                     <div className="details">
                         <div className="posterLogo">
-                            <img src="https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/5386/1375386-t-a7680d810a1b" alt="" />
+                            <img src="https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/4934/1594934-t-c2e816be5f21" alt="" />
                         </div>
                         <div className="timings">
                             <h3>2019 1h23m Hindi <span>U/A 16+</span></h3>
@@ -103,7 +106,31 @@ const HomeCarousel = () => {
                 </div>
                 <div class="carousel-item">
                     <div className="img">
-                    <img src="https://img10.hotstar.com/image/upload/f_auto,q_90,w_2048/sources/r1/cms/prod/2646/1572646-i-a71597762027" 
+                    <img src="https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/7516/1597516-i-751da827e22b" 
+                        alt="..." />
+                    </div>
+                    <div className="details">
+                        <div className="posterLogo">
+                            <img src="https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/2645/1572645-t-02f29ccbf461" alt="" />
+                        </div>
+                        <div className="timings">
+                            <h3>2019 1h23m Hindi <span>U/A 16+</span></h3>
+                        </div>
+                        <div className="desc">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, vel. Facilis impedit consectetur molestiae doloribus.</p>
+                        </div>
+                        <div className="genre">
+                            <p>Action &nbsp; | &nbsp;Spy&nbsp; | &nbsp;Buddy Cop&nbsp; | &nbsp;Chase</p>
+                        </div>
+                        <div className="button">
+                            <button className='watch-btn'> &#10148; &nbsp; Watch Now</button>
+                            <button className='add-btn'>+</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div className="img">
+                    <img src="https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/7516/1597516-i-751da827e22b" 
                         alt="..." />
                     </div>
                     <div className="details">
