3d9fb7fc4097ab43f795d64b0c4717fa050bafcc
Dhananjai <s.dhananjai7@gmail.com>
2023-09-26 16:58:06 +0530

    addeddescription box

--- Javascript/ContentCard.js
+++ Javascript/ContentCard.js
@@ -14,3 +14,19 @@
   } );
   
   contentSplide.mount();
+  
+  var contentSplide = new Splide( '#ContentSplide3', {
+    perPage: 7,
+    gap    : '.5rem',
+    padding: { right: '3rem' }
+  } );
+  
+  contentSplide.mount();
+  
+  var contentSplide = new Splide( '#ContentSplide4', {
+    perPage: 7,
+    gap    : '.5rem',
+    padding: { right: '3rem' }
+  } );
+  
+  contentSplide.mount();
